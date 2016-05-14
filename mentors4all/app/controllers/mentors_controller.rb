4f784b28d222d544ef6a3dbc4c55e127b61bcfd0
katushe <kathryngarbacz@dhcp67.chi.cleverbridge.com>
2016-05-14 16:13:56 -0500

    Add availabilities/subjects/grade levels to mentor show page

--- mentors4all/app/controllers/mentors_controller.rb
+++ mentors4all/app/controllers/mentors_controller.rb
@@ -16,7 +16,19 @@ class MentorsController < ApplicationController
   end
 
   def show
+    @mentor = Mentor.find(params[:id])
+    render json: {mentor: @mentor, availabilities: @mentor.availabilities, grade_levels: @mentor.grade_levels, subjects: @mentor.cs_subjects}
+  end
+
+  def update
+    @mentor = Mentor.find(params[:id])
+    @mentor.update_attributes(mentor_params)
+
     render json: {mentor: @mentor}
   end
 
+  private
+    def mentor_params
+      params.require(:mentor).permit(:location, :employment_status, :employment_location, :prior_experience, :courses_taken, :interest_in_mentoring, :example_explanation, :cs_expertise)
+    end
 end
