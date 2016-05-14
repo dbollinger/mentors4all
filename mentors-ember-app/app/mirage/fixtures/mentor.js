c4b30a588a862893161a2188bec7e9b480806ed3
Savannah Million <savannahmillion@gmail.com>
2016-05-14 18:00:50 -0500

    branch is demo ready:

--- mentors-ember-app/app/mirage/fixtures/mentor.js
+++ mentors-ember-app/app/mirage/fixtures/mentor.js
@@ -3,25 +3,37 @@ export default [
     id: 1,
     firstName: "Joe",
     lastName: "Smith",
+    description: "I'm a CS graduate student at Northwestern. I have two nieces and a nephew that are between 5 and 8 years old and I love teaching them to code!",
+    image:"http://loremflickr.com/300/300/scientist",
   }, {
     id: 2,
     firstName: "John",
-    lastName: "Smith"
+    lastName: "Smith",
+    description: "I'm a CS graduate student at Northwestern. I have two nieces and a nephew that are between 5 and 8 years old and I love teaching them to code!",
+    image:"http://loremflickr.com/300/300/engineer",
   }, {
     id: 3,
     firstName: "Joan",
-    lastName: "Rivers"
+    lastName: "Rivers",
+    description: "I'm a CS graduate student at Northwestern. I have two nieces and a nephew that are between 5 and 8 years old and I love teaching them to code!",
+    image:"http://loremflickr.com/300/300/lady",
   }, {
     id: 4,
     firstName: "Jim",
-    lastName: "Smith"
+    lastName: "Smith",
+    description: "I'm a CS graduate student at Northwestern. I have two nieces and a nephew that are between 5 and 8 years old and I love teaching them to code!",
+    image:"http://loremflickr.com/300/300/dude",
   }, {
     id: 5,
     firstName: "Jerry",
-    lastName: "Seinfeld"
+    lastName: "Seinfeld",
+    description: "I'm a CS graduate student at Northwestern. I have two nieces and a nephew that are between 5 and 8 years old and I love teaching them to code!",
+    image:"http://loremflickr.com/300/300/friend",
   }, {
     id: 6,
     firstName: "Jeff",
-    lastName: "Gordon"
+    lastName: "Gordon",
+    description: "I'm a CS graduate student at Northwestern. I have two nieces and a nephew that are between 5 and 8 years old and I love teaching them to code!",
+    image:"http://loremflickr.com/300/300/kid",
   },
 ];
--- mentors-ember-app/app/models/mentor.js
+++ mentors-ember-app/app/models/mentor.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
-  lastName: DS.attr('string')
+  lastName: DS.attr('string'),
+  description: DS.attr('string'),
+  image: DS.attr('string'),
 });
