4eee2a65ae67a7ef3967960e25a1dfcd9f085bc6
katushe <kathryngarbacz@dhcp67.chi.cleverbridge.com>
2016-05-14 13:51:02 -0500

    Add mentor & educator controllers

---
+++ mentors4all/app/controllers/educators_controller.rb
@@ -0,0 +1,2 @@
+class EducatorsController < ApplicationController
+end
--- mentors4all/test/models/mentor_contact_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MentorContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
