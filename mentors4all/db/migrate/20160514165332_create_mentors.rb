b210db7472b39364c3b59af191425925fce6452b
Caroline <kenworthyc@gmail.com>
2016-05-14 14:04:47 -0500

    Remove commas

--- mentors4all/db/migrate/20160514165332_create_mentors.rb
+++ mentors4all/db/migrate/20160514165332_create_mentors.rb
@@ -1,13 +1,13 @@
 class CreateMentors < ActiveRecord::Migration
   def change
     create_table :mentors do |t|
-      t.string, :location
-      t.string, :employment_status
-      t.string, :employment_location
-      t.text, :prior_experience
-      t.text, :courses_taken
-      t.text, :interest_in_mentoring
-      t.text, :example_explanation
+      t.string :location
+      t.string :employment_status
+      t.string :employment_location
+      t.text :prior_experience
+      t.text :courses_taken
+      t.text :interest_in_mentoring
+      t.text :example_explanation
       t.text :cs_expertise
 
       t.timestamps null: false
