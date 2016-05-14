4c79b5d847c38b09261367ff67a921b185bb1e5f
katushe <kathryngarbacz@dhcp67.chi.cleverbridge.com>
2016-05-14 13:05:23 -0500

    Create availability, grade level, subject models and join models

--- mentors4all/app/models/educator.rb
+++ mentors4all/app/models/educator.rb
@@ -1,3 +1,5 @@
 class Educator < ActiveRecord::Base
   has_one :user, as :contactable, dependent: :destroy
+  has_many :educator_subjects
+  has_many :cs_subjects, through: :educator_subjects
 end
--- mentors4all/app/models/mentor.rb
+++ mentors4all/app/models/mentor.rb
@@ -1,3 +1,5 @@
 class Mentor < ActiveRecord::Base
   has_one :user, as: :contactable, dependent: :destroy
+  has_many :mentor_subjects
+  has_many :cs_subjects, through: :mentor_subjects
 end
